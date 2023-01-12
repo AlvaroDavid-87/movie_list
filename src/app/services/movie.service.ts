856a232bf59a28e189eaa8005afeec59ce51bddc
Alvaro David Medel Espinoza <alvaro.david_87@zohomail.com>
2023-01-12 00:14:22 -0300

    bulma card for movie list

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -17,6 +17,7 @@ export class MovieService {
       new Movie("The Lord of the Rings: The Fellowship of the Ring", "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_.jpg",2001),
       new Movie("The Lord of the Rings: The Two Towers", "https://m.media-amazon.com/images/M/MV5BZGMxZTdjZmYtMmE2Ni00ZTdkLWI5NTgtNjlmMjBiNzU2MmI5XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",2002),
       new Movie("The Lord of the Rings: The Return of the King", "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX1000_.jpg",2003),
+      new Movie("The Batman", "https://m.media-amazon.com/images/M/MV5BYTExZTdhY2ItNGQ1YS00NjJlLWIxMjYtZTI1MzNlMzY0OTk4XkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_.jpg", 2022),
     ]
   }
 
